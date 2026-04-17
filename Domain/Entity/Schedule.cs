be4e5e3b264d67332a4c36830f47a8e63c428ccb
WeFake123 <augustoagi12345@gmail.com>
2026-04-17 19:56:42 -0300

    Creacion del Repositorio y Entidades part 2

---
+++ Domain/Entity/Schedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entity
+{
+    public class Schedule
+    {
+        public int Id { get; set; }
+        public enum Day{Lunes, Martes, Miercoles, Jueves, Viernes, Sabado, Domingo} 
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+        public int Id_Class { get; set; }
+
+    }
+}
---
+++ Domain/Interface/IScheduleRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Interface
+{
+    public interface IScheduleRepository
+    {
+
+        List<Schedule> GetAll();
+
+    }
+}
